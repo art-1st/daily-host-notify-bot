d884e9eaee3e647d62725f89609e18a5ffd576fb
art-1st <art-1st@naver.com>
2023-01-05 16:15:48 +0900

    👷: Lambda DynamoDB Role Test (6)

--- src/functions/slack/command/handler.ts
+++ src/functions/slack/command/handler.ts
@@ -1,21 +1,21 @@
-import { DynamoDB } from "@aws-sdk/client-dynamodb";
-import { marshall, unmarshall } from "@aws-sdk/util-dynamodb";
-import {
-  InvokeCommand,
-  InvokeCommandInput,
-  LambdaClient,
-} from "@aws-sdk/client-lambda";
+// import { DynamoDB } from "@aws-sdk/client-dynamodb";
+// import { marshall, unmarshall } from "@aws-sdk/util-dynamodb";
+// import {
+//   InvokeCommand,
+//   InvokeCommandInput,
+//   LambdaClient,
+// } from "@aws-sdk/client-lambda";
 import {
   formatJSONResponse,
   ValidatedEventAPIGatewayProxyEvent,
 } from "@libs/api-gateway";
 import { generateSlackBlockMessageResponse } from "@libs/message";
 // import { WebClient } from "@slack/web-api";
-import { TABLES } from "src/constants";
+// import { TABLES } from "src/constants";
 import { CommandRequestPayload } from "src/types/command";
 
 const {
-  AWS_REGION,
+  // AWS_REGION,
   SERVICE_NAME,
   SLACK_BOT_TOKEN,
   JIRA_KANBAN_NAME,
@@ -23,7 +23,7 @@ const {
 } = process.env;
 
 // const lambdaClient = new LambdaClient({ region: AWS_REGION });
-const ddbClient = new DynamoDB({ region: AWS_REGION });
+// const ddbClient = new DynamoDB({ region: AWS_REGION });
 
 const command: ValidatedEventAPIGatewayProxyEvent<any> = async (event) => {
   const { body } = event;
@@ -45,12 +45,6 @@ const command: ValidatedEventAPIGatewayProxyEvent<any> = async (event) => {
     return formatJSONResponse(400, {
       message: "Bad Request",
     });
-  }
-
-  const { Items } = await ddbClient.scan({ TableName: TABLES.USER });
-
-  if (Items) {
-    return formatJSONResponse(200, { items: Items });
   }
 
   const slackResponsePayload = body.split("=")[1];
